94d2ff3be06f730186f492c24f6d9dcf0e473694
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-04-01 17:27:52 +0900

    Update nvm version

--- 00install_nvm.sh
+++ 00install_nvm.sh
@@ -3,7 +3,6 @@
 echo "[Script begin] `date +%T`"
 
 # nodeのインストールのためにnvmをインストール
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 echo "ターミナルを一度終了して再度開いてください"
 echo "[Script finished] `date +%T`"
-
