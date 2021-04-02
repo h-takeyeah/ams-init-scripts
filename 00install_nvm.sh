11ad62176b5623d8c4c18581a024239c4d867718
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-04-02 12:35:54 +0900

    00install_nvmを修正

--- 00install_nvm.sh
+++ 00install_nvm.sh
@@ -7,7 +7,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # shell script内でnvm installすると上手くいかないので
 echo "以下のコマンドをコピペして実行してください"
-echo "export NVM_DIR="\$HOME/.config/nvm" && \ . "\$NVM_DIR/nvm.sh" \ nvm install --lts=Fermium --latest-npm"
+echo "export NVM_DIR=\"\$HOME/.config/nvm\" && . \"\$NVM_DIR/nvm.sh\" && nvm install --lts=Fermium --latest-npm"
 echo "終わったらターミナルを一度終了して再度開いてください"
 echo "その後01installer.shを実行してください"
 echo "[Script finished] `date +%T`"
